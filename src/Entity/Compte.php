b4818203f513027cfd5aafa38e150834f8bc5c64
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2023-02-12 10:36:56 +0100

    Ajout de l'entité Compte dans l'api

--- src/Entity/Compte.php
+++ src/Entity/Compte.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CompteRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -14,6 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CompteRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[ApiResource]
 class Compte implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
