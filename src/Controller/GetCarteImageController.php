7dacb15275c53c24f88ab0438a41b99d9cd8e2b8
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-02-12 19:29:12 +0100

    Ajout du controller en charge de récupérer l'image d'une carte sur le serveur et de la renvoyer

---
+++ src/Controller/GetCarteImageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Carte;
+use Symfony\Component\HttpFoundation\Response;
+
+class GetCarteImageController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    public function __invoke(Carte $data): Response
+    {
+        $imageName = $data->getContenuImage();
+        $imagePath = '/public/uploads/img/'.$imageName;
+        $image = file_get_contents($imagePath);
+
+        return new Response(
+            $image,
+            Response::HTTP_OK,
+            ['Content-Type' => 'image/png']
+        );
+    }
+}
