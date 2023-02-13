790b619d11842e0450adf68f9cd9eb46ceff407a
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2023-02-13 08:54:43 +0100

    ajout des groups pour chaque valeur d'un partenaire

--- src/Entity/Partenaire.php
+++ src/Entity/Partenaire.php
@@ -9,6 +9,7 @@ use App\Repository\PartenaireRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PartenaireRepository::class)]
 #[ApiResource(operations: [
@@ -26,12 +27,15 @@ class Partenaire
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups('get_carte')]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups('get_carte')]
     private ?string $nom = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups('get_carte')]
     private ?string $prenom = null;
 
     #[ORM\ManyToMany(targetEntity: Vigneron::class, mappedBy: 'partenaire')]
