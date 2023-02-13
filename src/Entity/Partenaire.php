176f05cb323d13553cc220e496cbd8b830169058
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2023-02-13 08:37:07 +0100

    implémentation d'apiPlatform

--- src/Entity/Partenaire.php
+++ src/Entity/Partenaire.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\PartenaireRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PartenaireRepository::class)]
+#[ApiResource]
 class Partenaire
 {
     #[ORM\Id]
