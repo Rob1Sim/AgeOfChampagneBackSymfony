f5a5004261c14ebbde53492191c06025b884fe20
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2023-02-13 11:17:00 +0100

    création du test getPartenaireByID qui vérifie si on peut récuperer les données d'un partenaire avec l'api

--- tests/Api/PartenaireGetCest.php
+++ tests/Api/PartenaireGetCest.php
@@ -2,6 +2,11 @@
 
 namespace App\Tests\Api;
 
+use App\Entity\Partenaire;
+use App\Factory\AnimationFactory;
+use App\Factory\PartenaireFactory;
+use App\Tests\ApiTester;
+
 class PartenaireGetCest
 {
     protected static function expectedProperties(): array
@@ -11,5 +16,23 @@ class PartenaireGetCest
             'nom' => 'string',
             'prenom' => 'string',
         ];
+    }
+
+    public function getPartenaireByID(ApiTester $I): void
+    {
+        $animation = AnimationFactory::createOne();
+        PartenaireFactory::createOne([
+            'nom' => 'test',
+            'prenom' => 'test',
+        ]);
+
+        $I->sendGet('/api/partenaires/1');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(Partenaire::class, '/api/partenaires/1');
+        $I->seeResponseIsAnItem(self::expectedProperties(), [
+            'nom' => 'test',
+            'prenom' => 'test',
+        ]);
     }
 }
