1bc4ccb8505e45bd8d3c016fbd930397f99fa0ab
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-02-14 17:07:17 +0100

    Création du fichier de tests sur Animation

---
+++ tests/Api/Animation/AnimationGetCest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Api\Animation;
+
+use App\Entity\Carte;
+use App\Factory\CarteFactory;
+use App\Factory\CruFactory;
+use App\Factory\VigneronFactory;
+use App\Tests\ApiTester;
+
+class AnimationGetCest
+{
+
+}
