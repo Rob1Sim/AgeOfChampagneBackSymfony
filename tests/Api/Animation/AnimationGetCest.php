2b4d1eed0e4ce451fa85561cddd7e1aecce2f984
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-02-14 17:42:07 +0100

    Création de la méthode de tests getAnimationById

--- tests/Api/Animation/AnimationGetCest.php
+++ tests/Api/Animation/AnimationGetCest.php
@@ -3,6 +3,8 @@
 namespace Api\Animation;
 
 use App\Entity\Animation;
+use App\Factory\AnimationFactory;
+use App\Tests\ApiTester;
 
 class AnimationGetCest
 {
@@ -12,12 +14,38 @@ class AnimationGetCest
             'id' => 'integer',
             'type' => 'string',
             'nom' => 'string',
-            'horaireDeb' => '\DateTimeInterface',
-            'horaireFin' => '\DateTimeInterface',
             'prix' => 'float',
+            'horaireDeb' => 'string',
+            'horaireFin' => 'string',
             'contenuImage' => 'string',
         ];
     }
 
+    public function getAnimationByID(ApiTester $I): void
+    {
+        $horaireDeb = new \DateTime();
+        $horaireFin = new \DateTime();
+        AnimationFactory::createOne([
+            'type' => 'test',
+            'nom' => 'test',
+            'prix' => 19.6,
+            'horaireDeb' => $horaireDeb,
+            'horaireFin' => $horaireFin,
+            'contenuImage' => 'feur',
+        ]);
 
+        $I->sendGet('/api/animations/1');
+
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(Animation::class, '/api/animations/1');
+        $I->seeResponseIsAnItem(self::expectedProperties(), [
+            'type' => 'test',
+            'nom' => 'test',
+            'prix' => 19.6,
+            'horaireDeb' => null,
+            'horaireFin' => null,
+            'contenuImage' => 'feur',
+        ]);
+    }
 }
