4d7f1032e3056d3e8b98b26801731aea402ebd14
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-02-23 17:21:33 +0100

    Première version du test pour la requete GET Cru : Pas encore testé (problème codeception)

--- tests/Api/Cru/CruGetCest.php
+++ tests/Api/Cru/CruGetCest.php
@@ -2,6 +2,11 @@
 
 namespace App\Tests\Api\Cru;
 
+use App\Entity\Cru;
+use App\Factory\CruFactory;
+use App\Factory\VigneronFactory;
+use App\Tests\ApiTester;
+
 class CruGetCest
 {
     protected static function expectedProperties(): array
@@ -14,5 +19,32 @@ class CruGetCest
             // blob ou string
             'infos' => 'string',
         ];
+    }
+
+    public function getCruDetail(ApiTester $I): void
+    {
+        $vigneron = VigneronFactory::createOne();
+        CruFactory::createOne([
+            'libelle' => 'lib1',
+            'horaire' => 'hor1',
+            'image' => 'img1',
+            'infos' => 'infos1',
+            'vigneron' => $vigneron,
+        ]);
+
+        $dataGet = [
+            'libelle' => 'lib1',
+            'horaire' => 'hor1',
+            'image' => 'img1',
+            'infos' => 'infos1',
+            'vigneron' => 1,
+        ];
+
+        $I->sendGet('/api/cru/1');
+
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(Cru::class, '/api/cru/1');
+        $I->seeResponseIsAnItem(self::expectedProperties(), $dataGet);
     }
 }
