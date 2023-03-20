c3c52e34c63888a784da1d6f38f2adc122db1d87
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-03-20 21:10:04 +0100

    Test pour le détail des crus par l'id : Test Fonctionnel

--- tests/Api/Cru/CruGetCest.php
+++ tests/Api/Cru/CruGetCest.php
@@ -4,7 +4,6 @@ namespace App\Tests\Api\Cru;
 
 use App\Entity\Cru;
 use App\Factory\CruFactory;
-use App\Factory\VigneronFactory;
 use App\Tests\ApiTester;
 
 class CruGetCest
@@ -15,36 +14,29 @@ class CruGetCest
             'id' => 'integer',
             'libelle' => 'string',
             'horaire' => 'string',
-            'image' => 'blob',
-            // blob ou string
             'infos' => 'string',
         ];
     }
 
     public function getCruDetail(ApiTester $I): void
     {
-        $vigneron = VigneronFactory::createOne();
         CruFactory::createOne([
             'libelle' => 'lib1',
             'horaire' => 'hor1',
-            'image' => 'img1',
             'infos' => 'infos1',
-            'vigneron' => $vigneron,
         ]);
 
         $dataGet = [
             'libelle' => 'lib1',
             'horaire' => 'hor1',
-            'image' => 'img1',
             'infos' => 'infos1',
-            'vigneron' => 1,
         ];
 
-        $I->sendGet('/api/cru/1');
+        $I->sendGet('/api/crus/1');
 
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseIsJson();
-        $I->seeResponseIsAnEntity(Cru::class, '/api/cru/1');
+        $I->seeResponseIsAnEntity(Cru::class, '/api/crus/1');
         $I->seeResponseIsAnItem(self::expectedProperties(), $dataGet);
     }
 }
