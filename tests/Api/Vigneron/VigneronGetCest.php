721c5d4bd0a3e8103c876e8d6a5593cf06f2a8fc
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-03-23 14:39:33 +0100

    Mise en place du test pour la requete Get Vigneron : OK

--- tests/Api/Vigneron/VigneronGetCest.php
+++ tests/Api/Vigneron/VigneronGetCest.php
@@ -2,6 +2,12 @@
 
 namespace App\Tests\Api\Vigneron;
 
+use App\Entity\Vigneron;
+use App\Factory\CruFactory;
+use App\Factory\ProduitFactory;
+use App\Factory\VigneronFactory;
+use App\Tests\ApiTester;
+
 class VigneronGetCest
 {
     protected static function expectedProperties(): array
@@ -17,5 +23,37 @@ class VigneronGetCest
             'produitID' => 'integer',
             'contenuImage' => 'string',
         ];
+    }
+
+    public function getVigneronByID(ApiTester $I): void
+    {
+        $produit = ProduitFactory::createOne();
+        $cru = CruFactory::createOne();
+        VigneronFactory::createOne([
+            'nom' => 'test',
+            'prenom' => 'test',
+            'adresse' => 'test',
+            'code_postal' => 'test',
+            'ville' => 'test',
+            'cru' => $cru,
+            'produit' => $produit,
+        ]);
+
+        $dataGet = [
+            'nom' => 'test',
+            'prenom' => 'test',
+            'adresse' => 'test',
+            'code_postal' => 'test',
+            'ville' => 'test',
+            'cruID' => 1,
+            'produitID' => 1,
+        ];
+
+        $I->sendGet('/api/vignerons/1');
+
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(Vigneron::class, '/api/vignerons/1');
+        $I->seeResponseIsAnItem(self::expectedProperties(), $dataGet);
     }
 }
