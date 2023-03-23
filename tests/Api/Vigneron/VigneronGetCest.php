9543fabbe53f0f1377519d3ce3a837986a18976c
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-03-23 14:04:54 +0100

    Ajout de la méthode expectedProperties dans VigneronGetCest

--- tests/Api/Vigneron/VigneronGetCest.php
+++ tests/Api/Vigneron/VigneronGetCest.php
@@ -4,5 +4,18 @@ namespace App\Tests\Api\Vigneron;
 
 class VigneronGetCest
 {
-
+    protected static function expectedProperties(): array
+    {
+        return [
+            'id' => 'integer',
+            'nom' => 'string',
+            'prenom' => 'string',
+            'adresse' => 'string',
+            'code_postal' => 'string',
+            'ville' => 'string',
+            'cruID' => 'integer',
+            'produitID' => 'integer',
+            'contenuImage' => 'string',
+        ];
+    }
 }
