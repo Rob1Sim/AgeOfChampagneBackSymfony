7311dadecd892651b1d7e49d4dca24d7137f1934
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2023-03-26 10:24:02 +0200

    Ajout de la méthode expectedProperties qui retourne les propriétés attendus

--- tests/Api/Compte/CompteGetMeCest.php
+++ tests/Api/Compte/CompteGetMeCest.php
@@ -4,5 +4,14 @@ namespace App\Tests\Api\Compte;
 
 class CompteGetMeCest
 {
-    
+    protected static function expectedProperties(): array
+    {
+        return [
+            'id' => 'integer',
+            'email' => 'string',
+            'roles' => 'string',
+            'date_naiss' => 'date',
+            'login' => 'string',
+        ];
+    }
 }
