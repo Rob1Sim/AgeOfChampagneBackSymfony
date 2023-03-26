046cff2ead2266b939a5096ca5343330c203c1d7
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2023-03-26 10:14:50 +0200

    Ajout du test pour récupérer les informations d'un utilisateur

--- tests/Api/Compte/CompteGetCest.php
+++ tests/Api/Compte/CompteGetCest.php
@@ -2,6 +2,10 @@
 
 namespace App\Tests\Api\Compte;
 
+use App\Entity\Compte;
+use App\Factory\CompteFactory;
+use App\Tests\ApiTester;
+
 class CompteGetCest
 {
     protected static function expectedProperties(): array
@@ -14,4 +18,25 @@ class CompteGetCest
             'login' => 'string',
         ];
     }
+
+    public function anonymousUserGetSimpleUserElement(ApiTester $I): void
+    {
+        // 1. 'Arrange'
+        $data = [
+            'login' => 'user1',
+            'email' => 'user-1@example.fr',
+        ];
+        CompteFactory::createOne($data);
+
+        // 2. 'Act'
+        $I->sendGet('/api/comptes/1');
+
+        // 3. 'Assert'
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(Compte::class, '/api/comptes/1');
+        $I->seeResponseIsAnItem(self::expectedProperties(), $data);
+    }
+    
+
 }
