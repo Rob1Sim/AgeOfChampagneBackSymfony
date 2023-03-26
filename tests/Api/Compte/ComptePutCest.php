f799feccaf5ea523739df29e65f1e1ccf36eb857
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2023-03-26 11:23:38 +0200

    Ajout du test permettant de vérifier qu'un utilisateur non connecté ne peut pas modifier de données

--- tests/Api/Compte/ComptePutCest.php
+++ tests/Api/Compte/ComptePutCest.php
@@ -2,6 +2,13 @@
 
 namespace App\Tests\Api\Compte;
 
+use App\Entity\Compte;
+use App\Factory\CompteFactory;
+use App\Tests\ApiTester;
+use Codeception\Attribute\DataProvider;
+use Codeception\Example;
+use Codeception\Util\HttpCode;
+
 class ComptePutCest
 {
     protected static function expectedProperties(): array
@@ -13,5 +20,17 @@ class ComptePutCest
             'date_naiss' => 'date',
             'login' => 'string',
         ];
+    }
+
+    public function anonymousUserForbiddenToPutUser(ApiTester $I): void
+    {
+        // 1. 'Arrange'
+        CompteFactory::createOne();
+
+        // 2. 'Act'
+        $I->sendPut('/api/comptes/1');
+
+        // 3. 'Assert'
+        $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
     }
 }
