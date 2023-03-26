de3a98000d07f8044f1afe50c991ea029a068604
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2023-03-26 10:32:45 +0200

    Ajout du test vérifiant bien qu'un utilisateur non connécté n'a pas accès au /me

--- tests/Api/Compte/CompteGetMeCest.php
+++ tests/Api/Compte/CompteGetMeCest.php
@@ -2,6 +2,10 @@
 
 namespace App\Tests\Api\Compte;
 
+use App\Factory\CompteFactory;
+use App\Tests\ApiTester;
+use Codeception\Util\HttpCode;
+
 class CompteGetMeCest
 {
     protected static function expectedProperties(): array
@@ -13,5 +17,17 @@ class CompteGetMeCest
             'date_naiss' => 'date',
             'login' => 'string',
         ];
+    }
+
+    public function anonymousMeIsUnauthorized(ApiTester $I): void
+    {
+        // 1. 'Arrange'
+        CompteFactory::createOne();
+
+        // 2. 'Act'
+        $I->sendGet('/api/me');
+
+        // 3. 'Assert'
+        $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
     }
 }
