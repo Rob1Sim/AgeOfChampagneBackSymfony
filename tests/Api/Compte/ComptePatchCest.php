2c95653f82de133f36159b9ee030dc39859901f5
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2023-03-26 10:56:22 +0200

    Ajout du test permettant de vérifier que l'utilisateur peut modifier son mdp

--- tests/Api/Compte/ComptePatchCest.php
+++ tests/Api/Compte/ComptePatchCest.php
@@ -71,5 +71,41 @@ class ComptePatchCest
         $I->seeResponseIsAnItem(self::expectedProperties(), $dataPatch);
     }
 
+    public function authenticatedUserCanChangeHisPassword(ApiTester $I): void
+    {
+        // 1. 'Arrange'
+        $dataInit = [
+            'login' => 'user1',
+            'password' => 'password',
+        ];
+        $user = CompteFactory::createOne($dataInit)->object();
+        $I->amLoggedInAs($user);
+
+        // 2. 'Act'
+        $dataPatch = ['password' => 'new password'];
+        $I->sendPatch('/api/comptes/1', $dataPatch);
+
+        // 3. 'Assert'
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(Compte::class, '/api/comptes/1');
+        $I->seeResponseIsAnItem(self::expectedProperties());
+
+        $I->amOnPage('/logout');
+        // Don't check response code since homepage is not configured (404)
+        // $I->seeResponseCodeIsSuccessful();
+        $I->amOnPage('/login');
+        $I->seeResponseCodeIsSuccessful();
+        $I->submitForm(
+            'form',
+            ['login' => 'user1', 'password' => 'new password'],
+            'Authentification'
+        );
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeInCurrentUrl('/api/docs');
+    }
+
+
+
 
 }
