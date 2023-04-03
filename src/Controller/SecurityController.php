71ed43f1cdb6c63896778dee7d11667246ca3436
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2023-04-03 20:39:19 +0200

    l'utilisateur connecté ne peut plus acceder à la page login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -12,6 +13,9 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return new RedirectResponse('http://localhost:5173/');
+        }
         // ToDo rediction vers la page d'accueil
 
         // get the login error if there is one
